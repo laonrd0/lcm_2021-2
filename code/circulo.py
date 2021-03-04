29cb9d9ce6ce9905c7cc32599d4847c2a11acecf
laonrd0 <laonrdo@cetus.localdomain>
2021-03-04 13:40:33 -0600

    add code

---
+++ code/circulo.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*
+import math
+
+
+r = float(input("Ingresa el valor del radio del círculo: "))
+
+a = math.pi * r**2
+
+print("El área del círculo es: ", a)
---
+++ code/hipotenusa.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+cateto_a = float(input("Introduce el valor del cateto a: "))
+cateto_b = float(input("Introduce el valor del cateto b: "))
+
+hipotenusa = (cateto_a ** 2 + cateto_b ** 2) ** 0.5
+
+print("el valor de la hipotenusa es: ", hipotenusa)
