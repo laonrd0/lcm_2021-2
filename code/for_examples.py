7057ae8e9a64feb7073ba96e15e29f476ed796bf
laonrd0 <laonrdo@cetus.localdomain>
2021-08-24 13:31:06 -0500

    add file

---
+++ code/for_examples.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*
+
+#Programa que imprime los primeros 10 números enteros
+
+objetos = [7, 'hola', True, 3.5]   #lista
+
+cadena_texto = "clase de python"  #cadena de texto
+
+for caracter in cadena_texto:
+    print(caracter)
